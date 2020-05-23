40fc8172c328b8d77d9257cacc813958aa93c77e
Peter <pfeerick@users.noreply.github.com>
2020-05-23 11:34:28 +1000

    Tweaked print statement + pylint changes
    Tweaked print statement for readability and made changes to make pylint happier

--- pine64-cpu-temp.py
+++ pine64-cpu-temp.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 from __future__ import print_function
 
-tempSource = "/sys/devices/virtual/thermal/thermal_zone0/temp"  # pine64
+TEMP_SOURCE = "/sys/devices/virtual/thermal/thermal_zone0/temp"  # pine64
 
-f = open(tempSource, "r")
-cpu_temp = int(f.read())
-f.close()
+F = open(TEMP_SOURCE, "r")
+CPU_TEMP = int(F.read())
+F.close()
 
-print("%.2f\u00b0C" % round(cpu_temp / 1000.0,2))
+print("{:.2f}\u00b0C".format(CPU_TEMP/1000.0))
